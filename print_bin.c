a4a5a0894bdb82aac69bd77c99b9159789069cca
johnenes <johneneojo1@gmail.com>
2023-10-17 12:54:10 -0700

    conversion specifiers:%i

---
+++ print_bin.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_bin - converts to binary
+ * @list_of_arg: argument
+ * Return: integer
+ */
+
+int print_bin(va_list list_of_arg)
+{
+    int count = 0;
+
+    unsigned int output = va_arg(list_of_arg, unsigned int);
+
+    for (int loop = 31; loop >= 0; loop--)
+    {
+        int bit = (output >> loop) & 1;
+        _putchar(bit + '0');
+        count++;
+    }
+
+    if (count == 0)
+    {
+        count++;
+        _putchar('0');
+    }
+
+    return count;
+}
+
